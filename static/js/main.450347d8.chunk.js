fa002fe55dd885653591c05943fd16d4e7d9d3c4
Jane Yushchenko <kelenella@ukr.net>
2021-11-09 01:06:40 +0200

    Updates

--- static/js/main.450347d8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-sandra-module-2"]=this["webpackJsonpreact-sandra-module-2"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),a=n(3),s=n.n(a),o=(n(8),n(9),n(0));var d=function(){return Object(o.jsx)("div",{className:"App"})};s.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(d,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.450347d8.chunk.js.map
